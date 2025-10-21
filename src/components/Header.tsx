5412d7bc8c65e6d9bfb7fb9900242941f5cb3da5
mrbobu-seo <bruno@knewledge.eu>
2025-10-21 09:51:48 +0200

    style: apply white filter to logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="bg-gray-900 text-white shadow-md">
       <div className="container mx-auto flex items-center justify-between p-4">
         <Link to="/">
-          <img src={logo} alt="Knewledge Logo" className="h-10" />
+          <img src={logo} alt="Knewledge Logo" className="h-10 brightness-0 invert" />
         </Link>
         <UserSession />
       </div>
